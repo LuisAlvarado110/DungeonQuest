796f186cd6d9e32bea3749db075f2fc2b7784a86
Charlie Odohui <carlosodohui@gmail.com>
2025-06-21 23:48:34 -0700

    Animaciones movimiento
    Se mejoró el sistema de movimiento preparandose para el desarrollo de ataques y otras acciones

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,16 +1,19 @@
 extends CharacterBody2D
 
 enum FACING {UP, DOWN, LEFT, RIGHT}
+enum PLAYER_STATE {IDLE, WALK_UP, WALK_DOWN, WALK_LEFT, WALK_RIGHT, ATTACK, DEATH}
 
 @onready var anim_sprite2d = $AnimatedSprite2D
 
 @export var move_spd = 15000;
-var facing_direction: FACING = FACING.UP
+var facing_direction: FACING = FACING.DOWN
+var current_state: PLAYER_STATE = PLAYER_STATE.IDLE
 var is_moving: bool = false
+var is_attacking: bool = false
 
 func _physics_process(delta: float) -> void:
 	player_input(delta)
-	set_animation()
+	calculate_state()
 	move_and_slide()
 
 func player_input(delta: float):
@@ -20,29 +23,73 @@ func player_input(delta: float):
 	velocity.x = move_spd * h_move * delta
 	velocity.y = move_spd * v_move * delta
 	
+	if velocity.y != 0:
+		if velocity.y > 0: facing_direction = FACING.DOWN
+		else: facing_direction = FACING.UP
+	if velocity.x != 0:
+		if velocity.x > 0: facing_direction = FACING.RIGHT
+		else: facing_direction = FACING.LEFT
+	
+	if Input.is_action_just_pressed("attack"):
+		is_attacking = true
+	
 	if (h_move != 0 and v_move != 0):
 		velocity /= sqrt(2)
 
-func set_animation():
-	if (velocity.length() == 0):
-		anim_sprite2d.stop()
-		return
-	
-	if (abs(velocity.x) > abs(velocity.y)):
-		if (velocity.x != 0):
-			if (velocity.x < 0):
-				facing_direction = FACING.LEFT
-				anim_sprite2d.play("walk_side")
-				anim_sprite2d.flip_h = true;
-			else:
-				facing_direction = FACING.RIGHT
-				anim_sprite2d.play("walk_side")
-				anim_sprite2d.flip_h = false;
+func calculate_state():
+	if velocity.length() != 0:
+		print("MOVING")
+		match facing_direction:
+			FACING.UP:
+				set_state(PLAYER_STATE.WALK_UP)
+			FACING.DOWN:
+				set_state(PLAYER_STATE.WALK_DOWN)
+			FACING.LEFT:
+				set_state(PLAYER_STATE.WALK_LEFT)
+			FACING.RIGHT:
+				set_state(PLAYER_STATE.WALK_RIGHT)
 	else:
-		if (velocity.y != 0):
-			if (velocity.y < 0):
-				facing_direction = FACING.UP
-				anim_sprite2d.play("walk_up")
-			else:
-				facing_direction = FACING.DOWN
-				anim_sprite2d.play("walk_down")
+		set_state(PLAYER_STATE.IDLE)
+		print("IDLE")
+
+func set_state(new_state: PLAYER_STATE):
+	if (new_state != current_state):
+		current_state = new_state
+		match current_state:
+			PLAYER_STATE.IDLE:
+				set_idle_animation()
+			PLAYER_STATE.WALK_UP:
+				set_walk_animation()
+			PLAYER_STATE.WALK_DOWN:
+				set_walk_animation()
+			PLAYER_STATE.WALK_LEFT:
+				set_walk_animation()
+			PLAYER_STATE.WALK_RIGHT:
+				set_walk_animation()
+			PLAYER_STATE.ATTACK:
+				pass
+
+func set_idle_animation():
+	anim_sprite2d.play("neutral")
+	match facing_direction:
+		FACING.UP:
+			anim_sprite2d.set_frame_and_progress(0,0.0)
+		FACING.DOWN:
+			anim_sprite2d.set_frame_and_progress(1,0.0)
+		FACING.LEFT:
+			anim_sprite2d.set_frame_and_progress(3,0.0)
+		FACING.RIGHT:
+			anim_sprite2d.set_frame_and_progress(3,0.0)
+
+func set_walk_animation():
+	match facing_direction:
+		FACING.UP:
+			anim_sprite2d.play("walk_up")
+		FACING.DOWN:
+			anim_sprite2d.play("walk_down")
+		FACING.LEFT:
+			anim_sprite2d.play("walk_side")
+			anim_sprite2d.flip_h = true;
+		FACING.RIGHT:
+			anim_sprite2d.play("walk_side")
+			anim_sprite2d.flip_h = false;
