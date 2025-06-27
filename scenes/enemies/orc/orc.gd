8fba9ae427f2424b8c2faef342a7170bf533700e
ChainRaccoon <luis.david.morales.luna@uabc.edu.mx>
2025-06-27 03:48:15 -0700

    mas enemigos

---
+++ scenes/enemies/orc/orc.gd
@@ -0,0 +1,20 @@
+extends BaseEnemy
+
+func _ready() -> void:
+	super._ready()
+	frame_attack = 3
+	damage = 3
+	movement_speed = 12000
+	hp = 6
+
+func _on_animated_sprite_2d_frame_changed() -> void:
+	if anim_sprite2d and anim_sprite2d.animation == "attack":
+		if anim_sprite2d.frame == frame_attack:
+			attack_area.monitoring = true
+			attack_area.visible = true
+			attack_area.monitorable = true
+			if attack_collision.position.x > 0 and anim_sprite2d.flip_h:
+				attack_collision.position.x *= -1
+			elif attack_collision.position.x < 0 and !anim_sprite2d.flip_h:
+				attack_collision.position.x = abs(attack_collision.position.x)
+			super.player_in_attack_area()
