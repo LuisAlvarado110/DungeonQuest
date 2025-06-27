73b146cac3a7cf3c7a4547a2d9943098d227fb44
Luis Alvarado <luis.alvarado11@uabc.edu.mx>
2025-06-27 01:11:40 -0700

    Implementado game over completo

--- scenes/canvas/game_over.gd
+++ scenes/canvas/game_over.gd
@@ -1,13 +1,25 @@
 extends CanvasLayer
 
+@onready var anim_player = $AnimationPlayer
+@onready var game_over_label = $%GameOver
+@onready var menu_btn = $%MenuBtn
+@onready var exit_btn = $%ExitBtn
+
 func _ready() -> void:
+	visible = false
+	menu_btn.visible = false
+	exit_btn.visible = false
 	SignalManager.on_player_death.connect(on_death_player)
 
 func on_death_player():
-	pass
+	get_tree().paused = false
+	visible = true
+	anim_player.play("game_over_anim")
+	menu_btn.visible = true
+	exit_btn.visible = true
 
 func _on_menu_btn_pressed() -> void:
-	pass # Replace with function body.
+	GameManager.on_main_menu()
 
 
 func _on_exit_btn_pressed() -> void:
