67da6e424dd2cb03bed9eb05f0ad596abc6c1ad2
Charlie Odohui <carlosodohui@gmail.com>
2025-06-27 02:27:08 -0700

    Variaciones piso 1 sur y este

--- singleton/NavigationManager.gd
+++ singleton/NavigationManager.gd
@@ -1,27 +1,5 @@
 extends Node
 
-const rooms: Dictionary = {
-	"scene_lvlgt1": preload("res://scenes/LevelGenTesting/LevelGenTesting1.tscn"),
-	
-	"floor1_spawn": preload("res://scenes/levels/floor1/spawn_room.tscn"),
-	
-	"west_testing1": preload("res://scenes/LevelGenTesting/WestRoom/WestTesting1.tscn"),
-	"west_testing2": preload("res://scenes/LevelGenTesting/WestRoom/WestTesting2.tscn"),
-	"west_testing3": preload("res://scenes/LevelGenTesting/WestRoom/WestTesting3.tscn"),
-	
-	"south_testing1": preload("res://scenes/LevelGenTesting/SouthRoom/SouthTesting1.tscn"),
-	"south_testing2": preload("res://scenes/LevelGenTesting/SouthRoom/SouthTesting2.tscn"),
-	"south_testing3": preload("res://scenes/LevelGenTesting/SouthRoom/SouthTesting3.tscn"),
-	
-	"east_testing1": preload("res://scenes/LevelGenTesting/EastRoom/EastTesting1.tscn"),
-	"east_testing2": preload("res://scenes/LevelGenTesting/EastRoom/EastTesting2.tscn"),
-	"east_testing3": preload("res://scenes/LevelGenTesting/EastRoom/EastTesting3.tscn"),
-	
-	"north_testing1": preload("res://scenes/LevelGenTesting/NorthRoom/NorthTesting1.tscn"),
-	"north_testing2": preload("res://scenes/LevelGenTesting/NorthRoom/NorthTesting2.tscn"),
-	"north_testing3": preload("res://scenes/LevelGenTesting/NorthRoom/NorthTesting3.tscn"),
-}
-
 var loaded_rooms: Dictionary
 
 signal on_trigger_player_spawn
@@ -51,7 +29,11 @@ func load_floor_rooms(floor: int):
 				"floor1_spawn": preload("res://scenes/levels/floor1/spawn_room.tscn"),
 				"west_1": preload("res://scenes/levels/floor1/west/floor_1_west.tscn"),
 				"east_1": preload("res://scenes/levels/floor1/east/floor1_east.tscn"),
+				"east_2": preload("res://scenes/levels/floor1/east/floor2_east.tscn"),
+				"east_3": preload("res://scenes/levels/floor1/east/floor3_east.tscn"),
 				"south_1": preload("res://scenes/levels/floor1/south/floor1_south.tscn"),
+				"south_2": preload("res://scenes/levels/floor1/south/floor2_south.tscn"),
+				"south_3": preload("res://scenes/levels/floor1/south/floor3_south.tscn"),
 				"north_1": preload("res://scenes/levels/floor1/north/floor1_north.tscn"),
 			}
 
